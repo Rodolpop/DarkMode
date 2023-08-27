7608654dd124b4adfab6c6233c77bba40174631e
rodolpop <garciasena@yahoo.com>
2023-08-27 18:49:35 +0200

    fin

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const inputEl = document.querySelector(".input");
 const bodyEl = document.querySelector("body");
 
 const h1El = document.querySelector("h1");
+
 const h2El = document.querySelector("h2");
+
+
 
 inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
@@ -12,9 +15,12 @@ updateBody()
 
 function updateBody(){
     if (inputEl.checked){
+
         bodyEl.style.background = "black";
         h1El.style.color = "black";
         h2El.style.color = "white";
+        
+        
         
     }else{
         
@@ -22,7 +28,8 @@ function updateBody(){
         h1El.style.color = "black";
         
         h2El.style.color = "white";
-
+        
+        
     }
 }
 
